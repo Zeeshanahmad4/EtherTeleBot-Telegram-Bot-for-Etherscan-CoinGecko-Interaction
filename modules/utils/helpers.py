006d33b98677b028be606eab0314de1d88f9943f
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 14:51:59 +0500

    Update helpers.py

--- modules/utils/helpers.py
+++ modules/utils/helpers.py
@@ -1,1 +1,17 @@
-# Helper functions and utilities 
+# modules/utils/helpers.py
+
+def validate_ethereum_address(address):
+    """
+    Validate if the given string is a potentially valid Ethereum address.
+    Note: This doesn't guarantee the address is active or has any balance, just that it's formatted correctly.
+    """
+    return address.startswith("0x") and len(address) == 42
+
+def truncate_string(input_string, length=10):
+    """
+    Truncate the given string to the specified length and append '...' if it's longer.
+    """
+    if len(input_string) > length:
+        return input_string[:length] + '...'
+    return input_string
+
