861a1e95ea03e46aa92db35d95f680277bd1aef0
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-31 14:51:34 +0500

    Update conversions.py

--- modules/utils/conversions.py
+++ modules/utils/conversions.py
@@ -1,1 +1,14 @@
-# Conversion functions 
+# modules/utils/conversions.py
+
+def wei_to_eth(wei_value):
+    """
+    Convert Wei to Ether. 1 Ether = 10^18 Wei.
+    """
+    return wei_value / (10 ** 18)
+
+def eth_to_wei(eth_value):
+    """
+    Convert Ether to Wei.
+    """
+    return eth_value * (10 ** 18)
+
